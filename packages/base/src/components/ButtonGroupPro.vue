eb4d66d09c811ab927a6f54164fc0d24ffbdd2de
ChenlingasMx <42308734@qq.com>
2024-03-20 16:51:56 +0800

    feat: 增加buttongrouppro

---
+++ packages/base/src/components/ButtonGroupPro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <el-button
+        v-for="(item, i) in button"
+        :key="i"
+        :type="item.type || 'primary'"
+        @click="item.click && item.click"
+        v-bind="item"
+      >
+        {{ item.label || "-" }}
+      </el-button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "buttonGroupPro",
+    props: {
+      button: {
+        type: Array,
+        default: () => [],
+      },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
--- packages/base/src/index.js
+++ packages/base/src/index.js
@@ -1,5 +1,6 @@
 import ProForm from './components/ProForm.vue';
 import ProTable from './components/ProTable.vue';
 import ProSelect from './components/ProSelect.vue';
+import ButtonGroupPro from './components/ButtonGroupPro.vue';
 
-export { ProForm, ProSelect, ProTable };
+export { ProForm, ProSelect, ProTable, ButtonGroupPro };
